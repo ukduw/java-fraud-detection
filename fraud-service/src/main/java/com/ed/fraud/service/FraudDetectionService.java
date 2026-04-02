0ab1d417a79e5def61b16a0b2fab8ffbe841921c
ukduw <edhkmayo@gmail.com>
2026-04-02 10:57:34 +0100

    added fraud detection service, basic, only one condition...

--- fraud-service/src/main/java/com/ed/fraud/service/FraudDetectionService.java
+++ fraud-service/src/main/java/com/ed/fraud/service/FraudDetectionService.java
@@ -1,4 +1,16 @@
 package com.ed.fraud.service;
 
+import com.ed.transaction_processing.common.event.TransactionEvent;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FraudDetectionService {
+    public void checkFraud(TransactionEvent event) {
+        if (event.getQty() > 10000) {
+            System.out.println("Fraud detected: " + event.getTransactionId());
+        } else {
+            System.out.println("Transaction valid: " + event.getTransactionId());
+        }
+    }
+
 }
