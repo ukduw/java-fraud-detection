d638b3bd91691cffb1b469df80f869587cec5927
ukduw <edhkmayo@gmail.com>
2026-04-02 10:58:47 +0100

    added kafka noti consumer

--- notification-service/src/main/java/com/ed/notification/kafka/NotificationConsumer.java
+++ notification-service/src/main/java/com/ed/notification/kafka/NotificationConsumer.java
@@ -1,4 +1,21 @@
 package com.ed.notification.kafka;
 
+import com.ed.transaction_processing.common.event.TransactionEvent;
+import com.ed.notification.service.NotificationService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NotificationConsumer {
+    private final NotificationService service;
+
+    public NotificationConsumer(NotificationService service) {
+        this.service = service;
+    }
+
+    @KafkaListener(topics="transactions", groupId="notification-group")
+    public void consume(TransactionEvent event) {
+        service.sendNotification(event);
+    }
+
 }
