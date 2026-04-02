0967f0dfc1433f98f6a58650768bfc95f9c4b077
ukduw <edhkmayo@gmail.com>
2026-04-02 10:38:59 +0100

    added kafka producer; TODO: add kafka dependency...

--- payment-service/src/main/java/com/ed/payment/kafka/TransactionProducer.java
+++ payment-service/src/main/java/com/ed/payment/kafka/TransactionProducer.java
@@ -1,4 +1,20 @@
 package com.ed.payment.kafka;
 
+import com.ed.transaction_processing.common.event.TransactionEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TransactionProducer {
+    private final KafkaTemplate<String, TransactionEvent> kafkaTemplate;
+
+    public TransactionProducer(KafkaTemplate<String, TransactionEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendTransaction(TransactionEvent event) {
+        kafkaTemplate.send("transactions", event.getTransactionId(), event);
+    }
+
+
 }
