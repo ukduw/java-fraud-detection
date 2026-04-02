3053e2a2a77cefb290644b1a6e9dc5e0b4b76c92
ukduw <edhkmayo@gmail.com>
2026-04-02 10:36:51 +0100

    added payment main app; springframework import fixed

--- payment-service/src/main/java/com/ed/payment/PaymentApplication.java
+++ payment-service/src/main/java/com/ed/payment/PaymentApplication.java
@@ -1,4 +1,12 @@
 package com.ed.payment;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
 }
