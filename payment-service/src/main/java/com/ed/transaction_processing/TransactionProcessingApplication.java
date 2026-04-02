c138d4304094ea88f0b3825270132e877ecf1347
ukduw <edhkmayo@gmail.com>
2026-04-02 09:43:11 +0100

    added module file structure per service + common, placeholders copied from default src/main/...

---
+++ payment-service/src/main/java/com/ed/transaction_processing/TransactionProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.ed.transaction_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionProcessingApplication.class, args);
+	}
+
+}
