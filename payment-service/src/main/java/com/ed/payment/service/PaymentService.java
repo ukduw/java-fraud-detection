d2127c6b2519122e66f6fa57b8135ac1980ffc76
ukduw <edhkmayo@gmail.com>
2026-04-02 10:38:34 +0100

    added payment service, imports fixed

--- payment-service/src/main/java/com/ed/payment/service/PaymentService.java
+++ payment-service/src/main/java/com/ed/payment/service/PaymentService.java
@@ -1,4 +1,19 @@
 package com.ed.payment.service;
 
+import com.ed.transaction_processing.common.event.TransactionEvent;
+import com.ed.payment.kafka.TransactionProducer;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentService {
+    private final TransactionProducer producer;
+
+    public PaymentService(TransactionProducer producer) {
+        this.producer = producer;
+    }
+
+    public void processPayment(TransactionEvent event) {
+        producer.sendTransaction(event);
+    }
+
 }
