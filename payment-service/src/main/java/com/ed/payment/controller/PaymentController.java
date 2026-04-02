ea1ddd63682935ca4658acb35965a72d3397c372
ukduw <edhkmayo@gmail.com>
2026-04-02 10:37:23 +0100

    added payment controller

--- payment-service/src/main/java/com/ed/payment/controller/PaymentController.java
+++ payment-service/src/main/java/com/ed/payment/controller/PaymentController.java
@@ -1,4 +1,23 @@
 package com.ed.payment.controller;
 
+import com.ed.transaction_processing.common.event.TransactionEvent;
+import com.ed.payment.service.PaymentService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payments")
 public class PaymentController {
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping
+    public String createPayment(@RequestBody TransactionEvent event) {
+        paymentService.processPayment(event);
+        return "Payment sent";
+    }
+
+
 }
