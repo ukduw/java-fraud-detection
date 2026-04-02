5b1626a6395ca062b8234e8840b35959910b089d
ukduw <edhkmayo@gmail.com>
2026-04-02 10:59:36 +0100

    added notification service - very basic, add dependency to send sms...? also, no db integration

--- notification-service/src/main/java/com/ed/notification/service/NotificationService.java
+++ notification-service/src/main/java/com/ed/notification/service/NotificationService.java
@@ -1,4 +1,11 @@
 package com.ed.notification.service;
 
+import com.ed.transaction_processing.common.event.TransactionEvent;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationService {
+    public void sendNotification(TransactionEvent event) {
+        System.out.println("Notifying user: " + event.getUserId());
+    }
 }
