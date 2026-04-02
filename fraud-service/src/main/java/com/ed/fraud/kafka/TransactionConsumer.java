173f0e666ccd9cdeacd7b86e3c122717317bb544
ukduw <edhkmayo@gmail.com>
2026-04-02 10:56:54 +0100

    added kafka fraud transaction consumer

--- fraud-service/src/main/java/com/ed/fraud/kafka/TransactionConsumer.java
+++ fraud-service/src/main/java/com/ed/fraud/kafka/TransactionConsumer.java
@@ -1,4 +1,21 @@
 package com.ed.fraud.kafka;
 
+import com.ed.transaction_processing.common.event.TransactionEvent;
+import com.ed.fraud.service.FraudDetectionService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TransactionConsumer {
+    private final FraudDetectionService fraudService;
+
+    public TransactionConsumer(FraudDetectionService fraudService) {
+        this.fraudService = fraudService;
+    }
+
+    @KafkaListener(topics="transactions", groupId="fraud-group")
+    public void consume(TransactionEvent event) {
+        fraudService.checkFraud(event);
+    }
+
 }
