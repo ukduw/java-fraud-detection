3f7547e1ae06dcc1063ac3f82887b90cdf41dad2
ukduw <edhkmayo@gmail.com>
2026-04-07 11:30:11 +0100

    added kafka config for payment service

---
+++ payment-service/src/main/java/com/ed/payment/config/KafkaConfig.java
@@ -0,0 +1,39 @@
+package com.ed.payment.config;
+
+import com.ed.transaction_processing.common.event.TransactionEvent;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+    // create topic automatically
+    @Bean
+    public NewTopic transactionsTopic() {
+        return new NewTopic("transactions", 1, (short) 1);
+    }
+
+    // producer config
+    @Bean
+    public ProducerFactory<String, TransactionEvent> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, TransactionEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
