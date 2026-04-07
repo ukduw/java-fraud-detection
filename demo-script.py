abc4c769d2a92d848abe48aa754d9be6ccd9573f
ukduw <edhkmayo@gmail.com>
2026-04-07 11:28:56 +0100

    added basic python demo script - needs fraud examples... will update after fraud detection logic more fleshed out

---
+++ demo-script.py
@@ -0,0 +1,9 @@
+import requests
+import random
+
+for i in range(10):
+    requests.post("http://localhost:8080/payments", json={
+        "transactionId": f"tx-{i}",
+        "userId": f"user-{i}",
+        "amount": random.randint(100, 20000)
+    })
