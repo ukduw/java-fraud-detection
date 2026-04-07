1e2be9bd9845ee924bf32108f51746face75bdc1
ukduw <edhkmayo@gmail.com>
2026-04-07 11:27:30 +0100

    added the SpringBootApplication initializations for fraud and notification services...

--- fraud-service/src/main/java/com/ed/fraud/FraudApplication.java
+++ fraud-service/src/main/java/com/ed/fraud/FraudApplication.java
@@ -1,4 +1,11 @@
 package com.ed.fraud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FraudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
 }
