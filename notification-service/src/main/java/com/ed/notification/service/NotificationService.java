f6656df798cd48135442e18a984811961d9b8c48
ukduw <edhkmayo@gmail.com>
2026-04-09 13:02:17 +0100

    updated status String -> TransactionStatus enum

--- notification-service/src/main/java/com/ed/notification/service/NotificationService.java
+++ notification-service/src/main/java/com/ed/notification/service/NotificationService.java
@@ -1,11 +1,24 @@
 package com.ed.notification.service;
 
 import com.ed.transaction_processing.common.event.TransactionEvent;
+import com.ed.transaction_processing.common.event.TransactionResultEvent;
+import com.ed.transaction_processing.common.model.TransactionStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
-    public void sendNotification(TransactionEvent event) {
-        System.out.println("Notifying user: " + event.getUserId());
+    public void handleResult(TransactionResultEvent event) {
+        switch (event.getStatus()) {
+            case TransactionStatus.APPROVED:
+                System.out.println("Payment approved for " + event.getUserId());
+                break;
+            case TransactionStatus.FLAGGED:
+                System.out.println("Flagged transaction for " + event.getUserId());
+                break;
+            case TransactionStatus.BLOCKED:
+                System.out.println("Blocked transaction for " + event.getUserId());
+        }
     }
 }
+
+// update to structured json logs ("event", "transactionId", "userId", "riskScore")
