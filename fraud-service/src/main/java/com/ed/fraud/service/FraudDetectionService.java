d5b43cdec257bd9cf0b4086feeac6a4213cc8669
ukduw <edhkmayo@gmail.com>
2026-04-09 13:00:59 +0100

    added score, abstracted checks to method, updated for TransactionStatus enum...

--- fraud-service/src/main/java/com/ed/fraud/service/FraudDetectionService.java
+++ fraud-service/src/main/java/com/ed/fraud/service/FraudDetectionService.java
@@ -1,21 +1,61 @@
 package com.ed.fraud.service;
 
+import com.ed.fraud.kafka.FraudResultProducer;
 import com.ed.fraud.model.UserTransaction;
 import com.ed.fraud.repository.UserTransactionRepository;
 import com.ed.transaction_processing.common.event.TransactionEvent;
+import com.ed.transaction_processing.common.event.TransactionResultEvent;
+import com.ed.transaction_processing.common.model.TransactionStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 
 @Service
 public class FraudDetectionService {
     private final UserTransactionRepository repo;
+    private final FraudResultProducer producer;
 
-    public FraudDetectionService(UserTransactionRepository repo) {
+    public FraudDetectionService(UserTransactionRepository repo, FraudResultProducer producer) {
         this.repo = repo;
+        this.producer = producer;
     }
 
     public void checkFraud(TransactionEvent event) {
+        int score = calculateRiskScore(event);
+
+        TransactionStatus status = TransactionStatus.APPROVED;
+        if (score >= 50 && score <= 69) {
+            status = TransactionStatus.FLAGGED;
+        }
+        if (score >= 70) {
+            status = TransactionStatus.BLOCKED;
+        }
+
+        // save transaction
+        repo.save(new UserTransaction(
+            event.getUserId(),
+            event.getQty(),
+            event.getLocation(),
+            event.getTimestamp(),
+            event.getTransactionStatus()
+        ));
+
+        TransactionResultEvent result = new TransactionResultEvent(
+            event.getTransactionId(),
+            event.getUserId(),
+            status,
+            score,
+            Instant.now()
+        );
+
+        producer.sendResult(result);
+    }
+
+    private int calculateRiskScore(TransactionEvent event) {
+        int score = 0;
+
+        // refactor to calculate score rather than print:
         if (event.getQty() > 10000) {
             System.out.println("Fraud detected: " + event.getTransactionId());
         } else {
@@ -37,13 +77,8 @@ public class FraudDetectionService {
             System.out.println("Fraud: high velocity transactions " + event.getUserId());
         }
 
-        // save transaction
-        repo.save(new UserTransaction(
-            event.getUserId(),
-            event.getQty(),
-            event.getLocation(),
-            event.getTimestamp()
-        ));
+
+        return score;
     }
 
 }
