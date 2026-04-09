5b7e95931166e580dfe87166f1868dab7a4ae95c
ukduw <edhkmayo@gmail.com>
2026-04-09 12:54:45 +0100

    added, updated for TransactionStatus enum, added table name...

--- fraud-service/src/main/java/com/ed/fraud/model/UserTransaction.java
+++ fraud-service/src/main/java/com/ed/fraud/model/UserTransaction.java
@@ -1,9 +1,11 @@
 package com.ed.fraud.model;
 
+import com.ed.transaction_processing.common.model.TransactionStatus;
 import jakarta.persistence.*;
 import java.time.Instant;
 
 @Entity
+@Table(name = "user_transactions")
 public class UserTransaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,13 +16,17 @@ public class UserTransaction {
     private String location;
     private Instant timestamp;
 
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+
     public UserTransaction() {}
 
-    public UserTransaction(String userId, double qty, String location, Instant timestamp) {
+    public UserTransaction(String userId, double qty, String location, Instant timestamp, TransactionStatus status) {
         this.userId = userId;
         this.qty = qty;
         this.location = location;
         this.timestamp = timestamp;
+        this.status = status;
     }
 
     // needs getters/setters
