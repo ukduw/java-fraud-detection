0c7126222687da73e08b73d1b24a048ef0e54e90
ukduw <edhkmayo@gmail.com>
2026-04-09 11:39:47 +0100

    added todo/note...

--- demo-script.py
+++ demo-script.py
@@ -10,3 +10,8 @@ for i in range(10):
         "qty": random.randint(100, 20000),
         "location": locations[random.randint(0, 5)]
     })
+
+
+# random location doesn't make sense
+# maybe small chance of location being changed - only that transaction should be flagged
+# or, write separate scripts to demonstrate different fraud logic
