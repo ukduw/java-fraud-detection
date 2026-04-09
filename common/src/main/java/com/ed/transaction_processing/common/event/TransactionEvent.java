90faf4cd7e280b52b25194dd7c15d62a0a2d2a65
ukduw <edhkmayo@gmail.com>
2026-04-09 09:26:17 +0100

    added timestamp to event

--- common/src/main/java/com/ed/transaction_processing/common/event/TransactionEvent.java
+++ common/src/main/java/com/ed/transaction_processing/common/event/TransactionEvent.java
@@ -1,18 +1,22 @@
 package com.ed.transaction_processing.common.event;
 
+import java.time.Instant;
+
 public class TransactionEvent {
 	private String transactionId;
 	private String userId;
 	private double qty;
 	private String location;
+	private Instant timestamp;
 
 	public TransactionEvent() {}
 
-	public TransactionEvent(String transactionId, String userId, double qty, String location) {
+	public TransactionEvent(String transactionId, String userId, double qty, String location, Instant timestamp) {
 		this.transactionId = transactionId;
 		this.userId = userId;
 		this.qty = qty;
 		this.location = location;
+		this.timestamp = timestamp;
 	}
 
 
@@ -26,6 +30,7 @@ public class TransactionEvent {
 		return qty;
 	}
 	public String getLocation() { return location; }
+	public Instant getTimestamp() { return timestamp; }
 		// note: use in FraudDetectionService logic
 
 	public void setTransactionId(String transactionId) {
@@ -38,4 +43,6 @@ public class TransactionEvent {
 		this.qty = qty;
 	}
 	public void setLocation(String location) { this.location = location; }
+	public void setTimestamp(Instant timestamp) { this.timestamp = timestamp; }
+		// in what scenarios does transaction timestamp need to be changed...?
 }
