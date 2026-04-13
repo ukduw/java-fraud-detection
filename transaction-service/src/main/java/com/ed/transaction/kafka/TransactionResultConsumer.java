4dc3390e9c8dddffbb187165349f3cf2acfd4ea5
ukduw <edhkmayo@gmail.com>
2026-04-13 10:39:56 +0100

    added transaction-service kafka consumer for transaction result

---
+++ transaction-service/src/main/java/com/ed/transaction/kafka/TransactionResultConsumer.java
@@ -0,0 +1,29 @@
+package com.ed.transaction.kafka;
+
+import com.ed.transaction.model.TransactionRecord;
+import com.ed.transaction.repository.TransactionRepository;
+import com.ed.transaction_processing.common.event.TransactionResultEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionResultConsumer {
+    private final TransactionRepository repo;
+
+    public TransactionResultConsumer(TransactionRepository repo) {
+        this.repo = repo;
+    }
+
+    @KafkaListener(topics = "transaction-results", groupId = "transaction-group")
+    public void consume(TransactionResultEvent event) {
+        TransactionRecord record = new TransactionRecord();
+
+        record.setTransactionId(event.getTransactionId());
+        record.setUserId(event.getUserId());
+        record.setStatus(event.getStatus());
+        record.setRiskScore(event.getRiskScore());
+        record.setTimestamp(event.getProcessedAt());
+
+        repo.save(record);
+    }
+}
