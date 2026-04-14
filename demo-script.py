0622bf063ebc1f35944cd62fc7cf37f8947bea33
ukduw <edhkmayo@gmail.com>
2026-04-14 12:23:36 +0100

    updated demo scripts - added fraud locations lists, sleeps, updated ranges so lists/db doesn't have to be so long..., lowered qty range for normal transactions, added fraud blocks for qty, location, high velocity

--- demo-script.py
+++ demo-script.py
@@ -2,16 +2,35 @@ import requests
 import random
 
 locations = ["London", "Frankfurt", "Paris", "Shanghai", "New York", "Dublin"]
+fraudcations = ["Stockholm", "Canberra", "Minsk", "Buenos Aires", "Kabul", "Copenhagen"]
 
-for i in range(10):
+for i in range(6):
     requests.post("http://localhost:8080/payments", json={
         "transactionId": f"tx-{i}",
         "userId": f"user-{i}",
-        "qty": random.randint(100, 20000),
-        "location": locations[random.randint(0, 5)]
+        "qty": random.randint(5, 100),
+        "location": locations[i-1]
+    })
+sleep(5)
+
+
+# fraud (qty, location)
+for i in range(6):
+    requests.post("http://localhost:8080/payments", json={
+        "transactionId": f"tx-{i+6}",
+        "userId": f"user-{i}",
+        "qty": random.randint(500, 10000),
+        "location": fraudcations[i-1]
+    })
+sleep(5)
+
+# high velocity test
+for i in range(5):
+    requests.post("http://localhost:8080/payments", json={
+        "transactionId": f"tx-{i+12}",
+        "userId": f"user-1",
+        "qty": random.randint(5, 1000),
+        "location": locations[0]
     })
 
 
-# random location doesn't make sense
-# maybe small chance of location being changed - only that transaction should be flagged
-# or, write separate scripts to demonstrate different fraud logic
