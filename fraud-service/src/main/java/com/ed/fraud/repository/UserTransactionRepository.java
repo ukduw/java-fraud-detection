ddf3afce60d4ac4db849f6123b1d2fc58ae96593
ukduw <edhkmayo@gmail.com>
2026-04-14 13:41:30 +0100

    added method to user transaction repo - queries db for user's transaction history; could use stdev instead...

--- fraud-service/src/main/java/com/ed/fraud/repository/UserTransactionRepository.java
+++ fraud-service/src/main/java/com/ed/fraud/repository/UserTransactionRepository.java
@@ -2,9 +2,14 @@ package com.ed.fraud.repository;
 
 import com.ed.fraud.model.UserTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserTransactionRepository extends JpaRepository<UserTransaction, Long> {
     List<UserTransaction> findTop5ByUserIdOrderByTimestampDesc(String userId);
+
+    @Query("SELECT COUNT(t), AVG(t.qty) FROM UserTransaction t WHERE t.userId = :userId")
+    Object[] findTransactionStatsByUserId(@Param("userId") String userId);
 }
