1060d0cad31474819868b1cc709a7a43ef800939
ukduw <edhkmayo@gmail.com>
2026-04-14 13:44:49 +0100

    updated fraud detection logic for both repos, added logic + risk scoring for abnormal qty relative to transaction history, unusual location vs homeLocation, fixed high velocity check for new repos, added prints... TODO: update TransactionStatus logic, add -1 condition

--- fraud-service/src/main/java/com/ed/fraud/service/FraudDetectionService.java
+++ fraud-service/src/main/java/com/ed/fraud/service/FraudDetectionService.java
@@ -1,7 +1,9 @@
 package com.ed.fraud.service;
 
 import com.ed.fraud.kafka.FraudResultProducer;
+import com.ed.fraud.model.User;
 import com.ed.fraud.model.UserTransaction;
+import com.ed.fraud.repository.UserRepository;
 import com.ed.fraud.repository.UserTransactionRepository;
 import com.ed.transaction_processing.common.event.TransactionEvent;
 import com.ed.transaction_processing.common.event.TransactionResultEvent;
@@ -13,12 +15,14 @@ import java.util.List;
 
 @Service
 public class FraudDetectionService {
-    private final UserTransactionRepository repo;
     private final FraudResultProducer producer;
+    private final UserRepository userRepo;
+    private final UserTransactionRepository txRepo;
 
-    public FraudDetectionService(UserTransactionRepository repo, FraudResultProducer producer) {
-        this.repo = repo;
+    public FraudDetectionService(FraudResultProducer producer, UserRepository userRepo, UserTransactionRepository txRepo) {
         this.producer = producer;
+        this.userRepo = userRepo;
+        this.txRepo = txRepo;
     }
 
     public void checkFraud(TransactionEvent event) {
@@ -33,7 +37,7 @@ public class FraudDetectionService {
         }
 
         // save transaction
-        repo.save(new UserTransaction(
+        txRepo.save(new UserTransaction(
             event.getUserId(),
             event.getQty(),
             event.getLocation(),
@@ -53,21 +57,43 @@ public class FraudDetectionService {
     }
 
     private int calculateRiskScore(TransactionEvent event) {
+        User user = userRepo.findById(event.getUserId()).orElse(null);
+        if (user == null) {
+            System.out.println("No such user");
+            return -1;
+        }
+
+
         int score = 0;
 
-        if (event.getQty() > 10000) {   // should query db for average qty; if different by order of magnitude, block
+        // qty
+        Object[] userTransactionStats = txRepo.findTransactionStatsByUserId(event.getUserId());
+        Long transactionCount = (Long) userTransactionStats[0];
+        Double avgSize = (Double) userTransactionStats[1];
+        if (avgSize != null && avgSize > 0 && transactionCount >= 3) {
+            double ratio = event.getQty() / avgSize;
+            if (ratio >= 100) { // should probably use stdev instead...
+                score += 70;
+                System.out.println("Abnormal size detected: needs manual approval");
+            } else if (ratio >= 10) {
+                score += 40;
+            }
+            System.out.println("AVG: " + avgSize + ", CURRENT: " + event.getQty());
+            }
+
+        // location
+        boolean unusualLocation = !event.getLocation().equalsIgnoreCase(user.getHomeLocation());
+        if (unusualLocation) {
             score += 70;
+            System.out.println("Unusual location detected: needs manual approval");
         }
 
-        List<UserTransaction> recent = repo.findTop5ByUserIdOrderByTimestampDesc(event.getUserId());
-        boolean newLocation = recent.stream().noneMatch(ut -> ut.getLocation().equals(event.getLocation()));
-        if (newLocation && !recent.isEmpty()) {
-            score += 70;
-        }
-
+        // high velocity
+        List<UserTransaction> recent = txRepo.findTop5ByUserIdOrderByTimestampDesc(event.getUserId());
         boolean highVelocity = recent.stream().anyMatch(ut -> ut.getTimestamp().isAfter(event.getTimestamp().minusSeconds(5)));
         if (highVelocity) {
             score += 50;
+            System.out.println("High transaction velocity detected: flagged");
         }
 
 
