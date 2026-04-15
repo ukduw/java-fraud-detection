bb05bd7b68ddfc7a25e4c3cbe5d7a93c0830b146
ukduw <edhkmayo@gmail.com>
2026-04-15 10:25:50 +0100

    updated from print to structured logging...

--- fraud-service/src/main/java/com/ed/fraud/service/FraudDetectionService.java
+++ fraud-service/src/main/java/com/ed/fraud/service/FraudDetectionService.java
@@ -8,6 +8,8 @@ import com.ed.fraud.repository.UserTransactionRepository;
 import com.ed.transaction_processing.common.event.TransactionEvent;
 import com.ed.transaction_processing.common.event.TransactionResultEvent;
 import com.ed.transaction_processing.common.model.TransactionStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -25,6 +27,9 @@ public class FraudDetectionService {
         this.txRepo = txRepo;
     }
 
+    private static final Logger log = LoggerFactory.getLogger(FraudDetectionService.class);
+
+
     public void checkFraud(TransactionEvent event) {
         int score = calculateRiskScore(event);
 
@@ -37,7 +42,8 @@ public class FraudDetectionService {
 
         if (score == -1) {
             status = TransactionStatus.BLOCKED;
-            System.out.println("No such user");
+            log.info("No such user userId={} transactionStatus={}",
+                    event.getUserId(), event.getTransactionStatus());
         }
 
 
@@ -62,9 +68,11 @@ public class FraudDetectionService {
     }
 
     private int calculateRiskScore(TransactionEvent event) {
+        log.info("Processing transactionId={} userId={} location={} qty={} timestamp={}",
+                event.getTransactionId(), event.getUserId(), event.getLocation(), event.getQty(), event.getTimestamp());
+
         User user = userRepo.findById(event.getUserId()).orElse(null);
         if (user == null) {
-            System.out.println("No such user");
             return -1;
         }
 
@@ -79,18 +87,19 @@ public class FraudDetectionService {
             double ratio = event.getQty() / avgSize;
             if (ratio >= 100) { // should probably use stdev instead...
                 score += 70;
-                System.out.println("Abnormal size detected: needs manual approval");
+                log.info("Abnormal size detected: needs manual approval transactionId={} userId={} avgQty={} currentQty={} timestamp={}",
+                        event.getTransactionStatus(), event.getUserId(), avgSize, event.getQty(), event.getTimestamp());
             } else if (ratio >= 10) {
                 score += 40;
             }
-            System.out.println("AVG: " + avgSize + ", CURRENT: " + event.getQty());
             }
 
         // location
         boolean unusualLocation = !event.getLocation().equalsIgnoreCase(user.getHomeLocation());
         if (unusualLocation) {
             score += 70;
-            System.out.println("Unusual location detected: needs manual approval");
+            log.info("Unusual location detected: needs manual approval transactionId={} userId={} usualLocation={} currentLocation={} qty={} timestamp={}",
+                    event.getTransactionId(), event.getUserId(), user.getHomeLocation(), event.getLocation(), event.getQty(), event.getTimestamp());
         }
 
         // high velocity
@@ -98,7 +107,8 @@ public class FraudDetectionService {
         boolean highVelocity = recent.stream().anyMatch(ut -> ut.getTimestamp().isAfter(event.getTimestamp().minusSeconds(5)));
         if (highVelocity) {
             score += 50;
-            System.out.println("High transaction velocity detected: flagged");
+            log.info("High transaction velocity detected: flagged - ending transaction transactionId={} userId={} location={} qty={} timestamp={}",
+                    event.getTransactionId(), event.getUserId(), event.getLocation(), event.getQty(), event.getTimestamp());
         }
 
 
