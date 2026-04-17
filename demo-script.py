af7147b9778a9c1015ae4374ec0183aeb1d106c6
ukduw <edhkmayo@gmail.com>
2026-04-17 12:14:50 +0100

    fixed script for request fields + incorrect python logic

--- demo-script.py
+++ demo-script.py
@@ -1,16 +1,19 @@
 import requests
 import random
 import time
+from datetime import datetime, timezone
 
 locations = ["London", "Frankfurt", "Paris", "Shanghai", "New York", "Dublin"]
 fraudcations = ["Stockholm", "Canberra", "Minsk", "Buenos Aires", "Kabul", "Copenhagen"]
 
 for i in range(6):
     requests.post("http://localhost:8080/payments", json={
-        "transactionId": f"tx-{i}",
-        "userId": f"user-{i}",
+        "transactionId": f"tx-{i+19}",
+        "userId": f"user-{i+1}",
         "qty": random.randint(5, 100),
-        "location": locations[i-1]
+        "location": locations[i],
+        "timestamp": datetime.now(timezone.utc).replace(microsecond=0).isoformat(),
+        "status": "PENDING"
     })
 time.sleep(5)
 
@@ -18,20 +21,24 @@ time.sleep(5)
 # fraud (qty, location)
 for i in range(6):
     requests.post("http://localhost:8080/payments", json={
-        "transactionId": f"tx-{i+6}",
-        "userId": f"user-{i}",
+        "transactionId": f"tx-{i+25}",
+        "userId": f"user-{i+1}",
         "qty": random.randint(500, 10000),
-        "location": fraudcations[i-1]
+        "location": fraudcations[i],
+        "timestamp": datetime.now(timezone.utc).replace(microsecond=0).isoformat(),
+        "status": "PENDING"
     })
 time.sleep(5)
 
 # high velocity test
 for i in range(5):
     requests.post("http://localhost:8080/payments", json={
-        "transactionId": f"tx-{i+12}",
+        "transactionId": f"tx-{i+31}",
         "userId": f"user-1",
         "qty": random.randint(5, 1000),
-        "location": locations[0]
+        "location": locations[0],
+        "timestamp": datetime.now(timezone.utc).replace(microsecond=0).isoformat(),
+        "status": "PENDING"
     })
 
 
