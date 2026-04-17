a774289a09d643d92af5443ac526bc946aa33218
ukduw <edhkmayo@gmail.com>
2026-04-17 12:17:59 +0100

    updated parameters to include qty, location to match request/db, reorganized, added to instantiation and getters

--- common/src/main/java/com/ed/transaction_processing/common/event/TransactionResultEvent.java
+++ common/src/main/java/com/ed/transaction_processing/common/event/TransactionResultEvent.java
@@ -7,25 +7,31 @@ import java.time.Instant;
 public class TransactionResultEvent {
     private String transactionId;
     private String userId;
+    private Double qty;
+    private String location;
+    private Instant processedAt;
     private TransactionStatus status; // APPROVED, FLAGGED, BLOCKED
     private int riskScore;
-    private Instant processedAt;
 
     public TransactionResultEvent() {}
 
-    public TransactionResultEvent(String transactionId, String userId, TransactionStatus status, int riskScore, Instant processedAt) {
+    public TransactionResultEvent(String transactionId, String userId, Double qty, String location, Instant processedAt, TransactionStatus status, int riskScore) {
         this.transactionId = transactionId;
         this.userId = userId;
+        this.qty = qty;
+        this.location = location;
+        this.processedAt = processedAt;
         this.status = status;
         this.riskScore = riskScore;
-        this.processedAt = processedAt;
     }
 
     public String getTransactionId() { return transactionId; }
     public String getUserId() { return userId; }
+    public Double getQty() { return qty; }
+    public String getLocation() { return location; }
+    public Instant getProcessedAt() { return processedAt; }
     public TransactionStatus getStatus() { return status; }
     public int getRiskScore() { return riskScore; }
-    public Instant getProcessedAt() { return processedAt; }
 
     // are setters needed...?
 }
